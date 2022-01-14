5782d6861232a8e75e3ad00ff920f515004ca0f8
Jim Billy <jimreevtech@gmail.com>
2022-01-14 23:33:47 +0530

    Updated version

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ terraform {
 
   required_providers {
     site24x7 = {
-      # source  = "site24x7/site24x7"
-      # // Update the latest version from https://registry.terraform.io/providers/site24x7/site24x7/latest 
-      # version = "0.0.1-beta.13"
+      source  = "site24x7/site24x7"
+      // Update the latest version from https://registry.terraform.io/providers/site24x7/site24x7/latest 
+      version = "0.0.1-beta.13"
       // Uncomment for local setup
       # source  = "registry.zoho.io/zoho/site24x7"
       # version = "1.0.0"
-      source  = "registry.terraform.io/site24x7/site24x7"
-      version = "1.0.0"
+      # source  = "registry.terraform.io/site24x7/site24x7"
+      # version = "1.0.0"
     }
   }
 }
