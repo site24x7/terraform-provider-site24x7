bd7cf3596f06fa760aac7fdb4eacc7688142ebc9
Jim Billy <jimreevtech@gmail.com>
2022-03-23 23:38:13 +0530

    Changed version

--- examples/all_resources_us.tf
+++ examples/all_resources_us.tf
@@ -6,12 +6,12 @@ terraform {
     site24x7 = {
       source  = "site24x7/site24x7"
       // Update the latest version from https://registry.terraform.io/providers/site24x7/site24x7/latest 
-      version = "1.0.8.1"
+      version = "1.0.9"
       // Uncomment for local setup
       # source  = "registry.zoho.io/zoho/site24x7"
-      # version = "1.0.8.1"
+      # version = "1.0.9"
     #   source  = "registry.terraform.io/site24x7/site24x7"
-    #   version = "1.0.8.1"
+    #   version = "1.0.9"
     }
   }
 }
