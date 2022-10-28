ebe017381e8b3d5451cd393303a8e3c560e3d69b
Vikram-chandran <vikramch7417vc@gmail.com>
2022-10-28 13:27:56 +0530

    telegram content change

--- api/endpoints/integration/telegram_impl_test.go
+++ api/endpoints/integration/telegram_impl_test.go
@@ -22,14 +22,14 @@ func TestTelegramIntegration(t *testing.T) {
 			Fn: func(t *testing.T, c rest.Client) {
 				telegramIntegration := &api.TelegramIntegration{
 					Name:          "Site24x7-Telegram Integration",
-					URL:           "https://web.telegram.org/z/#-87656875",
-					BotToken:	   "5334957868:ARTVWHmnzFiWfmzab-u-Ytrqs68dzom-TI8",
+					URL:           "https://web.tesdfm.org/z/#-12345",
+					BotToken:	   "53asdfg8:ART2345b-u-Ytdfgm-TI8",
 					SelectionType: 0,
 					TroubleAlert:  true,
 					CriticalAlert: false,
 					DownAlert:     false,
 					Title:         "$MONITOR_NAME is $STATUS",
-					AlertTagIDs:   []string{"113770000023231001"},
+					AlertTagIDs:   []string{"1234567801"},
 				}
 
 				_, err := NewTelegram(c).Create(telegramIntegration)
@@ -48,13 +48,13 @@ func TestTelegramIntegration(t *testing.T) {
 
 				expected := &api.TelegramIntegration{
 					Name:          "Site24x7-Telegram Integration",
-					URL:           "https://web.telegram.org/z/#-87656875",
-					BotToken:	   "5334957868:ARTVWHmnzFiWfmzab-u-Ytrqs68dzom-TI8",
-					ServiceID:     "435929000005306001",
+					URL:           "https://web.telegram.org/z/#-1234567",
+					BotToken:	   "1234567899",
+					ServiceID:     "1234567890",
 					ServiceStatus: 0,
 					SelectionType: 0,
 					Title:         "$MONITOR_NAME is $STATUS",
-					AlertTagIDs:   []string{"113770000023231001"},
+					AlertTagIDs:   []string{"12345678901"},
 				}
 
 				assert.Equal(t, expected, telegram_integration)
@@ -70,16 +70,16 @@ func TestTelegramIntegration(t *testing.T) {
 			Fn: func(t *testing.T, c rest.Client) {
 				telegram_integration := &api.TelegramIntegration{
 					Name:          "Site24x7-Telegram Integration",
-					URL:           "https://web.telegram.org/z/#-87656875",
-					BotToken:	   "5334957868:ARTVWHmnzFiWfmzab-u-Ytrqs68dzom-TI8",
+					URL:           "https://web.telegram.org/z/#-1234566",
+					BotToken:	   "1234567898765",
 					ServiceID:     "123",
 					SelectionType: 2,
 					TroubleAlert:  true,
 					CriticalAlert: false,
 					DownAlert:     false,
-					Monitors:      []string{"113770000023231032", "113770000023231043"},
+					Monitors:      []string{"123456789", "87654321"},
 					Title:         "$MONITOR_NAME is $STATUS",
-					AlertTagIDs:   []string{"113770000023231001", "113770000023231002"},
+					AlertTagIDs:   []string{"123456789", "87654321"},
 				}
 
 				_, err := NewTelegram(c).Update(telegram_integration)
