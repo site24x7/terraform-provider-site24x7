9850d430e69b0ac53c2221520ce2f6f8d6d272e3
sujithramagesh <sujithramageshrajan@gmail.com>
2024-10-25 10:41:05 +0530

    WebTransaction Document

--- docs/resources/web_transaction_browser_monitor.md
+++ docs/resources/web_transaction_browser_monitor.md
@@ -25,11 +25,17 @@ Use this resource to create, upda te and delete a Web-Transaction-Browser monito
     // (Required) Unique name to be used in the ping URL.
     base_url: "https://www.example.com"
 
-    //(Optional)Recorded transaction script type.(txt , side)
+    //(Required while creation)Recorded transaction script type.(txt , side)
     selenium_script="{\"id\":\"b500a6da-dbb7-4d0c-968e-ae3fd6ab411f\",\"version\":\"1.1\",\"name\":\"\",       \"url\":\"https://www.example.com\",\"tests\":[{\"id\":\"f30156f4-3a70-4031-9a41-dbe8eac7e494\",\"name\":\"\",\"commands\":[{\"id\":\"59b9fca1-28f3-46eb-afc5-0be35a1f582f\",\"comment\":\"\",\"command\":\"newStep\",\"target\":\"Loading - https://www.example.com\",\"targets\":[],\"value\":\"\",\"URL\":\"https://www.example.com\",\"stepCount\":1,\"stepTime\":\"0\",\"actionName\":\"\"},{\"id\":\"14bbdd0e-f78f-4591-916e-4b6cf94ce576\",\"comment\":\"\",\"command\":\"open\",\"target\":\"/\",\"targets\":[],\"value\":\"\",\"URL\":\"\",\"stepCount\":0,\"stepTime\":0,\"actionName\":\"\"}]}],\"suites\":[{\"id\":\"38e2db3f-b835-47e8-8786-b87792d6fe4f\",\"name\":\"\",\"persistSession\":false,\"parallel\":false,\"timeout\":300,\"tests\":[\"f30156f4-3a70-4031-9a41-dbe8eac7e494\"]}],\"urls\":[\"https://www.example.com/\"],\"plugins\":[]}"
 
-    //(Optional)Recorded transaction script type.(txt , side)
+    //(Required while creation)Recorded transaction script type.(txt , side)
     script_type="txt"
+
+    //(Optional)Map of String
+    proxy_details={webProxyUrl: "qwerty", webProxyUname: "sdcsdcsdc", webProxyPass: "sadasds"}
+
+    //(Optional)Map of auth-details
+    auth_details={userName: "12345",  password: "12345"}
 
     //(Optional)Check interval for monitoring.
     check_frequency="15"
@@ -79,6 +85,26 @@ Use this resource to create, upda te and delete a Web-Transaction-Browser monito
       "456"
     ]
 
+  // (Optional) List of dependent resource IDs. Suppress alert when dependent monitor(s) is down.
+  dependency_resource_ids = [
+    "123",
+    "456"
+  ]
+    // (Optional) Location Profile to be associated with the monitor. If 
+    // location_profile_id and location_profile_name are omitted,
+    // the first profile returned by the /api/location_profiles endpoint
+    // (https://www.site24x7.com/help/api/#list-of-all-location-profiles) will be
+    // used.
+    location_profile_id = "123"
+
+    // (Optional) Name of the Location Profile that has to be associated with the monitor. 
+    // Either specify location_profile_id or location_profile_name.
+    // If location_profile_id and location_profile_name are omitted,
+    // the first profile returned by the /api/location_profiles endpoint
+    // (https://www.site24x7.com/help/api/#list-of-all-location-profiles) will be
+    // used.
+    location_profile_name = "North America"
+
     // (Optional) List if user group IDs to be notified on down. 
     // Either specify user_group_ids or user_group_names. If omitted, the
     // first user group returned by the /api/user_groups endpoint
@@ -122,39 +148,40 @@ Use this resource to create, upda te and delete a Web-Transaction-Browser monito
 
 ### Required
 * `base_url`     (String) BaseURL of the transaction.
-* `selenium_script`(String)Recorded Trasanction script to create a monitor.
-* `script_type`   (String)Recorded transaction script type.(txt , side)
-* `proxy_details` (List of String) Proxy checking in the web response
+* `selenium_script`(String)Recorded Trasanction script to create a monitor.(Required for creation)
+* `script_type`   (String)Recorded transaction script type.(txt , side)(Required for creation)
 * `cookies` (Map of String) Cookies for Advanced settings
 ### Optional
 * `id` (String) The ID of this resource.
 * `display_name` (String) Display Name for the monitor.
 * `type`         (String) REALBROWSER.
 * `check_frequency` (String)Check interval for monitoring.
-* `threshold_profile_id` (String) Threshold profile to be associated with the monitor.
-* `page_load_time` (Number)Timeout for page load.
-* `async_dc_enabled` (Boolean) When asynchronous data collection is enabled, polling will be carried out from all the locations at the same time. If it is disabled, polling will be done consecutively from the selected locations.
-* `think_time`  (Number)Think time between each steps.
-<!-- * `parallel_polling` Enable parallel polling to initiate data collection from all the configured monitoring locations simultaneously during hourly polls -->
-* `perform_automation` (Boolean) Check box to do automation or not
 * `resolution` (String) Screen resolution for running the script.
 * `browser_type` (Number) Type of the browser. 1 - Firefox, 2 - Chrome. Default value is 1.
 * `browser_version` (Number) Version of the browser. 83 - Firefox, 88 - Chrome. Default value is 83.
+* `proxy_details` (Map of String) Proxy checking in the web response
+
+* `page_load_time` (Number)Timeout for page load.
+* `async_dc_enabled` (Boolean) When asynchronous data collection is enabled, polling will be carried out from all the locations at the same time. If it is disabled, polling will be done consecutively from the selected locations.
+* `think_time`  (Number)Think time between each steps.
+* `auth_details` (Map of String) auth details to access the monitor details
 * `custom_headers` (Map of String) A Map of Header name and value.
 * `user_agent` (String) User Agent to be used while monitoring the website.
-* `auth_pass` (String) Authentication password to access the website.
-* `auth_user` (String) Authentication user name to access the website.
-* `credential_profile_id` (String)Credential Profile to associate the website with. Notes: If you're using Auth user and Auth password, you can't configure Credential Profile
-* `ip_type` (Boolean)Whether ipv6 or ipv4
+* `ip_type` (Number)Whether ipv6 or ipv4
 * `ignore_cert_err` (Boolean) ssl certificate configuration for the monitor
+* `perform_automation` (Boolean) Check box to do automation or not
+* `threshold_profile_id` (String) Threshold profile to be associated with the monitor.
 * `notification_profile_id` (String) Notification profile to be associated with the monitor. Either specify notification_profile_id or notification_profile_name. If notification_profile_id and notification_profile_name are omitted, the first profile returned by the /api/notification_profiles endpoint will be used.
 * `notification_profile_name` (String) Name of the notification profile to be associated with the monitor. Profile name matching works for both exact and partial match.
+* `location_profile_id` (String) Location profile to be associated with the monitor.
+* `location_profile_name` (String) Name of the location profile to be associated with the monitor.
 * `monitor_groups` (List of String) List of monitor groups to which the monitor has to be associated.
 * `user_group_ids` (List of String) List of user groups to be notified when the monitor is down. Either specify user_group_ids or user_group_names. If omitted, the first user group returned by the /api/user_groups endpoint will be used.
 * `user_group_names` (List of String) List of user group names to be notified when the monitor is down. Either specify user_group_ids or user_group_names. If omitted, the first user group returned by the /api/user_groups endpoint will be used.
 * `tag_ids` (List of String) List of tags IDs to be associated to the monitor. Either specify tag_ids or tag_names.
 * `tag_names` (List of String) List of tag names to be associated to the monitor. Tag name matching works for both exact and partial match. Either specify tag_ids or tag_names.
+* `dependency_resource_ids` (List of String) List of dependent resource IDs. Suppress alert when dependent monitor(s) is down.
 * `third_party_service_ids` (List of String) List of Third Party Service IDs to be associated to the monitor.
 * `on_call_schedule_id` (String) Mandatory, if the user group ID is not given. On-Call Schedule ID of your choice.
-
+* `actions` (Map of String) Action to be performed on monitor IT Automation templates. 
 Refer [API documentation](https://www.site24x7.com/help/api/#web-transaction-(browser)) for more information about attributes.
--- examples/web_transaction_browser_monitor_us.tf
+++ examples/web_transaction_browser_monitor_us.tf
@@ -51,129 +51,132 @@ provider "site24x7" {
 // Site24x7 Domain Expiry monitor API doc - https://www.site24x7.com/help/api/#web-transaction-(browser)
 resource "site24x7_web_transaction_browser_monitor" "web_transaction_browser_monitor_basic" {
 
-    // (Optional)Display name for the monitor
-     display_name="RBM-Terraform"
-
-    //(Optional) Toggle to enable asynchronous data collection or not
-     async_dc_enabled = false
+    
      
-    //(Required)Base url for the monitor
-      base_url= "https://www.demoqa.com/"
+    // (Required)Base url for the monitor
+       base_url= "https://www.demoqa.com/"
 
 
-    //(Required)Selenium script to add to the monitor
-      selenium_script="{\"id\":\"b500a6da-dbb7-4d0c-968e-ae3fd6ab411f\",\"version\":\"1.1\",\"name\":\"\",\"url\":\"https://www.example.com\",\"tests\":[{\"id\":\"f30156f4-3a70-4031-9a41-dbe8eac7e494\",\"name\":\"\",\"commands\":[{\"id\":\"59b9fca1-28f3-46eb-afc5-0be35a1f582f\",\"comment\":\"\",\"command\":\"newStep\",\"target\":\"Loading - https://www.example.com\",\"targets\":[],\"value\":\"\",\"URL\":\"https://www.example.com\",\"stepCount\":1,\"stepTime\":\"0\",\"actionName\":\"\"},{\"id\":\"14bbdd0e-f78f-4591-916e-4b6cf94ce576\",\"comment\":\"\",\"command\":\"open\",\"target\":\"/\",\"targets\":[],\"value\":\"\",\"URL\":\"\",\"stepCount\":0,\"stepTime\":0,\"actionName\":\"\"}]}],\"suites\":[{\"id\":\"38e2db3f-b835-47e8-8786-b87792d6fe4f\",\"name\":\"\",\"persistSession\":false,\"parallel\":false,\"timeout\":300,\"tests\":[\"f30156f4-3a70-4031-9a41-dbe8eac7e494\"]}],\"urls\":[\"https://www.example.com/\"],\"plugins\":[]}"
+    // (Required while creation)Selenium script to add to the monitor
+       selenium_script="{\"id\":\"b500a6da-dbb7-4d0c-968e-ae3fd6ab411f\",\"version\":\"1.1\",\"name\":\"\",\"url\":\"https://www.example.com\",\"tests\":[{\"id\":\"f30156f4-3a70-4031-9a41-dbe8eac7e494\",\"name\":\"\",\"commands\":[{\"id\":\"59b9fca1-28f3-46eb-afc5-0be35a1f582f\",\"comment\":\"\",\"command\":\"newStep\",\"target\":\"Loading - https://www.example.com\",\"targets\":[],\"value\":\"\",\"URL\":\"https://www.example.com\",\"stepCount\":1,\"stepTime\":\"0\",\"actionName\":\"\"},{\"id\":\"14bbdd0e-f78f-4591-916e-4b6cf94ce576\",\"comment\":\"\",\"command\":\"open\",\"target\":\"/\",\"targets\":[],\"value\":\"\",\"URL\":\"\",\"stepCount\":0,\"stepTime\":0,\"actionName\":\"\"}]}],\"suites\":[{\"id\":\"38e2db3f-b835-47e8-8786-b87792d6fe4f\",\"name\":\"\",\"persistSession\":false,\"parallel\":false,\"timeout\":300,\"tests\":[\"f30156f4-3a70-4031-9a41-dbe8eac7e494\"]}],\"urls\":[\"https://www.example.com/\"],\"plugins\":[]}"
 
-    //(Required)Script type of the selenium script added
-      script_type="txt"
+    // (Required while creation)Script type of the selenium script added
+       script_type="side"
+    // (Required for updation) Provide the Cookies for parameter forwarding in Map format.
+       cookies ={
+          "Accept" = "application/json"
+        }
+
+    // (Optional)Display name for the monitor
+       display_name="RBM-Terraform"
+
+    // (Optional) Toggle to enable asynchronous data collection or not
+       async_dc_enabled = false
 
     // (Optional) Check interval for monitoring. Default: 1. See
     // https://www.site24x7.com/help/api/#check-interval for all supported
     // values.
-      check_frequency = "1"
+       check_frequency = "1"
 
-    //(Optional) browser version of the monitor 
-     browser_version=10101
+    // (Optional) browser version of the monitor 
+       browser_version=10101
 
-    //(Optional) Think time of the monitor
-     think_time=3
+    // (Optional) Think time of the monitor
+       think_time=3
 
 
-     //(Optional)Page loading time
-     page_load_time=60
+    // (Optional)Page loading time
+       page_load_time=60
 
-    //(Optional)Resolution of the monitor
-     resolution="1600,900"
+    // (Optional)Resolution of the monitor
+       resolution="1600,900"
 
-    //(Optional)Ip type of the monitor
-      ip_type = 2
+    // (Optional)Ip type of the monitor
+       ip_type = 2
 
-    //(Optional) Browser type of the monitor
-      browser_type = 1
+    // (Optional) Browser type of the monitor
+       browser_type = 1
 
     // (Optional) List of dependent resource IDs. Suppress alert when dependent monitor(s) is down.
-      dependency_resource_ids = [
+       dependency_resource_ids = [
         "123",
         "456"
-      ]
+       ]
 
     // (Optional) Provide the Custom Header for parameter forwarding in Map format.
-      custom_headers ={
+       custom_headers ={
         "Accept" = "application/json"
-      }
+       }
 
-    // (Required) Provide the Cookies for parameter forwarding in Map format.
-      cookies ={
-        "Accept" = "application/json"
-      }
+    
 
-    //(Required) Map of proxy-details 
-      proxy_details={webProxyUrl: "qwerty", webProxyUname: "sdcsdcsdc", webProxyPass: "sadasds"}
+    // (Optional) Map of proxy-details 
+       proxy_details={webProxyUrl: "qwerty", webProxyUname: "sdcsdcsdc", webProxyPass: "sadasds"}
 
-    //(Optional)Map of auth-details
-      auth_details={userName: "12345",  password: "12345"}
+    // (Optional)Map of auth-details
+       auth_details={userName: "12345",  password: "12345"}
 
     // (Optional) User Agent to be used while monitoring the website.
-      user_agent = "some user agent string"
+       user_agent = "some user agent string"
 
     // (Optional) Threshold profile to be associated with the monitor. If
     // omitted, the first profile returned by the /api/threshold_profiles
     // endpoint for the website monitor (https://www.site24x7.com/help/api/#list-threshold-profiles) will
     // be used.
-      threshold_profile_id = "123"
+       threshold_profile_id = "123"
 
-    //(Optional)Toggle button to perform automation or not
-      perform_automation=true
+    // (Optional)Toggle button to perform automation or not
+       perform_automation=true
 
-    //(Optional)if user_group_ids is not choosen
-    //On-Call Schedule of your choice.
-    //Create new On-Call Schedule or find your preferred On-Call Schedule ID.
-    on_call_schedule_id="456418000001258016"
-
-
-  // (Optional) Location Profile to be associated with the monitor. If 
-  // location_profile_id and location_profile_name are omitted,
-  // the first profile returned by the /api/location_profiles endpoint
-  // (https://www.site24x7.com/help/api/#list-of-all-location-profiles) will be
-  // used.
-    location_profile_id = "123"
-
-  // (Optional) Name of the Location Profile that has to be associated with the monitor. 
-  // Either specify location_profile_id or location_profile_name.
-  // If location_profile_id and location_profile_name are omitted,
-  // the first profile returned by the /api/location_profiles endpoint
-  // (https://www.site24x7.com/help/api/#list-of-all-location-profiles) will be
-  // used.
-    location_profile_name = "North America"
+    // (Optional)if user_group_ids is not choosen
+    // On-Call Schedule of your choice.
+    // Create new On-Call Schedule or find your preferred On-Call Schedule ID.
+       on_call_schedule_id="456418000001258016"
 
 
-  // (Optional) Map of status to actions that should be performed on monitor
-  // status changes. See
-  // https://www.site24x7.com/help/api/#action-rule-constants for all available
-  // status values.
-    actions = {1=465545643755}
+    // (Optional) Location Profile to be associated with the monitor. If 
+    // location_profile_id and location_profile_name are omitted,
+    // the first profile returned by the /api/location_profiles endpoint
+    // (https://www.site24x7.com/help/api/#list-of-all-location-profiles) will be
+    // used.
+       location_profile_id = "123"
+
+    // (Optional) Name of the Location Profile that has to be associated with the monitor. 
+    // Either specify location_profile_id or location_profile_name.
+    // If location_profile_id and location_profile_name are omitted,
+    // the first profile returned by the /api/location_profiles endpoint
+    // (https://www.site24x7.com/help/api/#list-of-all-location-profiles) will be
+    // used.
+       location_profile_name = "North America"
 
 
-  // (Optional) Notification profile to be associated with the monitor. If
-  // omitted, the first profile returned by the /api/notification_profiles
-  // endpoint (https://www.site24x7.com/help/api/#list-notification-profiles)
-  // will be used.
-    notification_profile_id = "123"
+    // (Optional) Map of status to actions that should be performed on monitor
+    // status changes. See
+    // https://www.site24x7.com/help/api/#action-rule-constants for all available
+    // status values.
+       actions = {1=465545643755}
 
-  // (Optional) Name of the notification profile that has to be associated with the monitor.
-  // Profile name matching works for both exact and partial match.
-  // Either specify notification_profile_id or notification_profile_name.
-  // If notification_profile_id and notification_profile_name are omitted,
-  // the first profile returned by the /api/notification_profiles endpoint
-  // (https://www.site24x7.com/help/api/#list-notification-profiles) will be
-  // used.
-    notification_profile_name = "Terraform Profile"
 
-  // (Optional) List of monitor group IDs to associate the monitor to.
-    monitor_groups = [
-      "123",
-      "456"
-    ]
+    // (Optional) Notification profile to be associated with the monitor. If
+    // omitted, the first profile returned by the /api/notification_profiles
+    // endpoint (https://www.site24x7.com/help/api/#list-notification-profiles)
+    // will be used.
+       notification_profile_id = "123"
+
+    // (Optional) Name of the notification profile that has to be associated with the monitor.
+    // Profile name matching works for both exact and partial match.
+    // Either specify notification_profile_id or notification_profile_name.
+    // If notification_profile_id and notification_profile_name are omitted,
+    // the first profile returned by the /api/notification_profiles endpoint
+    // (https://www.site24x7.com/help/api/#list-notification-profiles) will be
+    // used.
+      notification_profile_name = "Terraform Profile"
+
+    // (Optional) List of monitor group IDs to associate the monitor to.
+      monitor_groups = [
+        "123",
+        "456"
+      ]
 
   // (Optional) List if user group IDs to be notified on down. 
   // Either specify user_group_ids or user_group_names. If omitted, the
@@ -218,19 +221,15 @@ resource "site24x7_web_transaction_browser_monitor" "web_transaction_browser_mon
       base_url= "https://www.example.com/"
 
 
-    //(Required)Selenium script to add to the monitor
+    //(Required while creation)Selenium script to add to the monitor
       selenium_script="{\"id\":\"b500a6da-dbb7-4d0c-968e-ae3fd6ab411f\",\"version\":\"1.1\",\"name\":\"\",\"url\":\"https://www.example.com\",\"tests\":[{\"id\":\"f30156f4-3a70-4031-9a41-dbe8eac7e494\",\"name\":\"\",\"commands\":[{\"id\":\"59b9fca1-28f3-46eb-afc5-0be35a1f582f\",\"comment\":\"\",\"command\":\"newStep\",\"target\":\"Loading - https://www.example.com\",\"targets\":[],\"value\":\"\",\"URL\":\"https://www.example.com\",\"stepCount\":1,\"stepTime\":\"0\",\"actionName\":\"\"},{\"id\":\"14bbdd0e-f78f-4591-916e-4b6cf94ce576\",\"comment\":\"\",\"command\":\"open\",\"target\":\"/\",\"targets\":[],\"value\":\"\",\"URL\":\"\",\"stepCount\":0,\"stepTime\":0,\"actionName\":\"\"}]}],\"suites\":[{\"id\":\"38e2db3f-b835-47e8-8786-b87792d6fe4f\",\"name\":\"\",\"persistSession\":false,\"parallel\":false,\"timeout\":300,\"tests\":[\"f30156f4-3a70-4031-9a41-dbe8eac7e494\"]}],\"urls\":[\"https://www.example.com/\"],\"plugins\":[]}"
 
-    //(Required)Script type of the selenium script added
-      script_type="txt"
+    //(Required while creation)Script type of the selenium script added
+      script_type="side"
 
-
-    // (Required) Provide the Cookies for parameter forwarding in Map format.
-      cookies ={
-        "Accept" = "application/json"
-      }
-
-    //(Required) Map of proxy-details 
-      proxy_details={webProxyUrl: "qwerty", webProxyUname: "sdcsdcsdc", webProxyPass: "sadasds"}
+    // (Required for updation) Provide the Cookies for parameter forwarding in Map format.
+       cookies ={
+          "Accept" = "application/json"
+        }
 
 }
