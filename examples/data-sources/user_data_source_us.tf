1e943f6f261a34fc3ef9098ab72c1071e8dd5ebe
Vinothdarwin <k.vinoth1007309@gmail.com>
2024-10-29 14:58:28 +0530

    Handling the User data source in Site24x7 user terraform resource.

--- examples/data-sources/user_data_source_us.tf
+++ examples/data-sources/user_data_source_us.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     site24x7 = {
       source  = "site24x7/site24x7"
+      //version = "1.0.92"
       # Update the latest version from https://registry.terraform.io/providers/site24x7/site24x7/latest 
       
     }
   }
 }
-
 // Authentication API doc - https://www.site24x7.com/help/api/#authentication
 provider "site24x7" {
   // (Security recommendation - It is always best practice to store your credentials in a Vault of your choice.)
@@ -47,72 +47,39 @@ provider "site24x7" {
   
   }
 
+
 // Data source to fetch a user group
-data "site24x7_user_group" "s247usergroup" {
+data "site24x7_user" "s247user" {
   // (Required) Regular expression denoting the name of the user group.
-  name_regex = "a"
-  
+  name_regex = "vinoth"  
 }
 
 // Displays the User Group ID
-output "s247_user_group_id" {
+output "s247_user_id" {
   description = "User group ID : "
-  value       = data.site24x7_user_group.s247usergroup.id
+  value       = data.site24x7_user.s247user.id
 }
 
-// Displays the User Group Name
-output "s247_user_group_name" {
-  description = "User group name : "
-  value       = data.site24x7_user_group.s247usergroup.display_name
+// Displays the User Group ID
+output "s247_user_email" {
+  description = "Email : "
+  value       = data.site24x7_user.s247user.email
 }
 
-// Displays the matching usergroup IDs
-output "s247_matching_ids" {
-  description = "Matching user group IDs : "
-  value       = data.site24x7_user_group.s247usergroup.matching_ids
+// Displays the User Role
+output "s247_user_displayname" {
+  description = "Display_Name : "
+  value       = data.site24x7_user.s247user.display_name
 }
 
-// Displays the matching usergroup IDs and names
-output "s247_matching_ids_and_names" {
-  description = "Matching user group IDs and names : "
-  value       = data.site24x7_user_group.s247usergroup.matching_ids_and_names
+// Displays the User Role
+output "s247_user_matchingUserIDs" {
+  description = "Role : "
+  value       = data.site24x7_user.s247user.matching_ids
 }
 
-// Displays the Users
-output "s247_usergroup_users" {
-  description = "Users : "
-  value       = data.site24x7_user_group.s247usergroup.users
+output "s247_user_matchingUserIDsAndNames" {
+  description = "Role : "
+  value       = data.site24x7_user.s247user.matching_ids_and_names
 }
-
-// Displays the attribute group ID
-output "s247_attribute_group_id" {
-  description = "Attribute group ID : "
-  value       = data.site24x7_user_group.s247usergroup.attribute_group_id
-}
-
-// Displays the product ID
-output "s247_product_id" {
-  description = "Product ID : "
-  value       = data.site24x7_user_group.s247usergroup.product_id
-}
-
-// Iterating the user group data source
-data "site24x7_user_group" "usergrouplist" {
-  for_each = toset(["e", "a"])
-  name_regex = each.key
-}
-
-locals {
-  user_group_ids = toset([for prof in data.site24x7_user_group.usergrouplist : prof.id])
-  user_group_names = toset([for prof in data.site24x7_user_group.usergrouplist : prof.display_name])
-}
-
-output "s247_user_group_ids" {
-  description = "Matching user group IDs : "
-  value       = local.user_group_ids
-}
-
-output "s247_user_group_names" {
-  description = "Matching user group names : "
-  value       = local.user_group_names
-}
+//
